00f0589b79407d34d94efd0d3711b0df55486b42
Zynd4x <kneckebrotman42@googlemail.com>
2026-02-03 19:12:57 +0100

    asd

---
+++ Home.js
@@ -0,0 +1,35 @@
+const hoursElement = document.getElementById("hours");
+const minutesElement = document.getElementById("minutes");
+const secondsElement = document.getElementById("seconds");
+const pause = document.getElementById("pauseBtn");
+const schwach = document.getElementById("schwachBtn");
+let totalSeconds = 0;
+let timerInterval;
+
+
+pause.addEventListener("click", function() {
+    if (!timerInterval) {
+        timerInterval = setInterval(setTime, 1000);
+    }    
+})
+
+schwach.addEventListener("click", function(){
+    clearInterval(timerInterval);
+    timerInterval = null;
+    totalSeconds = 0;
+    secondsElement.innerHTML = "00";
+    minutesElement.innerHTML = "00";
+    hoursElement.innerHTML = "00";
+})
+
+function setTime() {
+    ++totalSeconds;
+    secondsElement.innerHTML = pad(totalSeconds % 60)
+    minutesElement.innerHTML = pad(Math.floor(totalSeconds / 60) % 60)
+    hoursElement.innerHTML = pad(Math.floor(totalSeconds / 3600) % 24)
+}
+
+function pad(val) {
+    return val < 10 ? "0" + val : val;
+}
+
